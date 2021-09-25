845a0bc1df5f82fff0cb0b34ea9a0269c191705e
shettykaran57 <shettykaran57@yahoo.com>
2021-09-26 01:02:34 +0530

    Waiting for postgres to start and setup has completed

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +83,6 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
     }
-}
 }
 
 
